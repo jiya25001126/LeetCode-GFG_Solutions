56c9027c701b8a15662531360783abfd3b5be2c6
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-13 12:40:38 +0530

    Added solution - LeetHub

---
+++ 700226-square-root-GFG/700226-square-root.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        int low = 0, high = n;
+        int ans = 0;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if ((long long)mid * mid == n) {
+                return mid;
+            }
+            else if ((long long)mid * mid < n) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
