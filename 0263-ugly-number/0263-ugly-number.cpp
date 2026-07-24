5c7c3f80afcc46511b09ddba3818b60704824656
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-24 23:41:48 +0530

    Time: 0 ms (100%), Space: 7.8 MB (44.64%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n<=0) return false;
+        while(n%2==0)
+            n /= 2;
+        while(n%3==0)
+            n /= 3;
+        while(n%5==0)
+            n /= 5;
+        return n==1;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
