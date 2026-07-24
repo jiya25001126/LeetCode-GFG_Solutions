583a3589d7362780a7cf83ec48521213194c43a5
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-24 23:55:44 +0530

    Time: 0 ms (100%), Space: 7.7 MB (53.16%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0292-nim-game/0292-nim-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4 != 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
