6ca0f37e23e922eeba1545ae15f3c4d1da2b998c
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-25 23:47:45 +0530

    Time: 0 ms (100%), Space: 10.6 MB (79.92%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
