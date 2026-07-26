ea6f1c553246acbaa495b01fb7b655f2000761d7
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-26 23:16:30 +0530

    Time: 0 ms (100%), Space: 15 MB (17.61%) - LeetHub - Time: O(n + m), Space: O(n)

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp;
+        vector<int> ans;
+
+        for (int num : nums1) {
+            mp[num]++;
+        }
+
+        for (int num : nums2) {
+            if (mp[num] > 0) {
+                ans.push_back(num);
+                mp[num]--;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
