7b5e097d0c256d08b64553d034ff31077fef4601
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-26 23:48:42 +0530

    Time: 0 ms (100%), Space: 13 MB (47.62%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+
+        for (int num : nums) {
+            if (num == first || num == second || num == third)
+                continue;
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+
+        return (third == LLONG_MIN) ? first : third;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
