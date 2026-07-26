5b1c34a064f651ad86e8ef367ebfdc8fcb24740c
JIYA JAISWAL <jiya.25001126@kiet.edu>
2026-07-26 23:58:34 +0530

    Time: 0 ms (100%), Space: 8.2 MB (84.53%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i] != ' ' && (i==0 || s[i-1] == ' ')) count++;
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
